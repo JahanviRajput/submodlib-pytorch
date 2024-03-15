daca1f59e154d42081e7e1d357ac964ffebccb1f
JahanviRajput <142418693+JahanviRajput@users.noreply.github.com>
2024-03-15 15:17:36 +0530

    Update DisparityMin.py

--- pytorch/submod/DisparityMin.py
+++ pytorch/submod/DisparityMin.py
@@ -2,12 +2,15 @@ import numpy as np
 import scipy
 from helper import *
 from typing import Set
+import torch
 from ..SetFunction import SetFunction
 
 class DisparityMinFunction(SetFunction):
 
-	def __init__(self, n, mode, sijs=None, data=None, metric="cosine", num_neighbors=None):
+	def __init__(self, n, mode, sijs=None, data=None, metric="cosine", num_neighbors=None, batch_size=0):
+
 		super(DisparityMinFunction, self).__init__()
+		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 		self.n = n
 		self.mode = mode
 		self.metric = metric
@@ -18,13 +21,13 @@ class DisparityMinFunction(SetFunction):
 		self.cpp_sijs = None
 		self.cpp_content = None
 		self.effective_ground_set = None
+		self.batch_size=batch_size
 
 		if self.n <= 0:
 			raise Exception("ERROR: Number of elements in ground set must be positive")
 
 		if self.mode not in ['dense', 'sparse']:
 			raise Exception("ERROR: Incorrect mode. Must be one of 'dense' or 'sparse'")
-
 		if type(self.sijs) != type(None): # User has provided similarity kernel
 			if type(self.sijs) == scipy.sparse.csr.csr_matrix:
 				if num_neighbors is None or num_neighbors <= 0:
@@ -36,24 +39,29 @@ class DisparityMinFunction(SetFunction):
 					raise Exception("ERROR: Dense kernel provided, but mode is not dense")
 			else:
 				raise Exception("Invalid kernel provided")
-			#TODO: is the below dimensionality check valid for both dense and sparse kernels?
 			if np.shape(self.sijs)[0]!=self.n or np.shape(self.sijs)[1]!=self.n:
 				raise Exception("ERROR: Inconsistentcy between n and dimensionality of given similarity kernel")
 			if type(self.data) != type(None):
 				print("WARNING: similarity kernel found. Provided data matrix will be ignored.")
 
-		else: #similarity kernel has not been provided
+		else: 
 			if type(self.data) != type(None):
 				if np.shape(self.data)[0]!=self.n:
 					raise Exception("ERROR: Inconsistentcy between n and no of examples in the given data matrix")
+
 				if self.mode == "dense":
 					if self.num_neighbors  is not None:
 						raise Exception("num_neighbors wrongly provided for dense mode")
 					self.num_neighbors = np.shape(self.data)[0] #Using all data as num_neighbors in case of dense mode
-				self.cpp_content = np.array(create_kernel(X = torch.tensor(self.data.tolist()), metric = self.metric, num_neigh = self.num_neighbors, mode = self.mode).to_dense())
-				val = self.cpp_content[0]
-				row = list(self.cpp_content[1].astype(int))
-				col = list(self.cpp_content[2].astype(int))
+				
+				self.data = map(np.ndarray.tolist,self.data)
+				self.data = list(self.data)
+				y = torch.tensor(self.data)
+				z = y.to(device="cuda")
+				x= create_kernel(X=z, metric=self.metric, num_neigh=self.num_neighbors, mode=self.mode, batch=self.batch_size)
+				val = x[0].cpu().detach().numpy()
+				row = list(x[1].cpu().detach().numpy().astype(int))
+				col = list(x[2].cpu().detach().numpy().astype(int))
 				if self.mode=="dense":
 					self.sijs = np.zeros((n,n))
 					self.sijs[row,col] = val
@@ -64,7 +72,6 @@ class DisparityMinFunction(SetFunction):
 
 		cpp_ground_sub = {-1} #Provide a dummy set for pybind11 binding to be successful
 
-		#Breaking similarity matrix to simpler native data structures for implicit pybind11 binding
 		if self.mode=="dense":
 			self.cpp_sijs = self.sijs.tolist() #break numpy ndarray to native list of list datastructure
 			if type(self.cpp_sijs[0])==int or type(self.cpp_sijs[0])==float: #Its critical that we pass a list of list to pybind11
@@ -76,19 +83,21 @@ class DisparityMinFunction(SetFunction):
 			self.numeffectivegroundset  = len(self.effective_ground_set)
 			self.currentMin = 0
 
+
+
 		if self.mode=="sparse": #break scipy sparse matrix to native component lists (for csr implementation)
 			self.cpp_sijs = {}
 			self.cpp_sijs['arr_val'] = self.sijs.data.tolist() #contains non-zero values in matrix (row major traversal)
 			self.cpp_sijs['arr_count'] = self.sijs.indptr.tolist() #cumulitive count of non-zero elements upto but not including current row
-			self.cpp_sijs['arr_col'] = self.sijs.indices.tolist() #contains col index corrosponding to non-zero values in arr_val														n,           arr_val                    arr_count            			arr_col
+			self.cpp_sijs['arr_col'] = self.sijs.indices.tolist() #contains col index corrosponding to non-zero values in arr_val
+			# self.cpp_obj = DisparityMin(self.n, self.cpp_sijs['arr_val'], self.cpp_sijs['arr_count'], self.cpp_sijs['arr_col'])
+			#																n,           arr_val                    arr_count            			arr_col
 			if(len(self.cpp_sijs['arr_val']) ==0 or len(self.cpp_sijs['arr_count']) ==0 or len(self.cpp_sijs['arr_col']) ==0):
 				raise Exception("Error: Empty/Corrupt sparse similarity kernel")
 			self.sparse_kernel = subcp.SparseSim(self.cpp_sijs['arr_val'],self.cpp_sijs['arr_count'],self.cpp_sijs['arr_col'])
 			self.effective_ground_set = set(range(n))
 			self.numeffectivegroundset = len(self.effective_ground_set)
 			self.currentMin = 0
-
-
 
 	def evaluate(self, X: Set[int]) -> float:
 		effective_X = X
@@ -105,7 +114,6 @@ class DisparityMinFunction(SetFunction):
 		return self.currentMin
 
 	def get_effective_ground_set(self) -> Set[int]:
-
 		return self.effective_ground_set
 
 	def marginal_gain(self, X: Set[int], item: int) -> float:
@@ -138,8 +146,8 @@ class DisparityMinFunction(SetFunction):
 			if enable_checks and item in effective_X:
 					return 0.0
 
-			if False and item not in self.effective_ground_set:
-					return 0.0
+			# if False and item not in self.effective_ground_set:
+			# 		return 0.0
 
 			min_val = 1.0 if len(effective_X) == 1 else self.currentMin
 
@@ -155,6 +163,9 @@ class DisparityMinFunction(SetFunction):
 					raise ValueError("Error: Only dense and sparse mode supported")
 
 			return min_val - self.currentMin
+
+    return gains
+
 
 	def update_memoization(self, X: Set[int], item: int) -> None:
 			effective_X = X
